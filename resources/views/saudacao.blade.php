5e31781ce165717843d076e4745d9424beb0919c
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-30 22:32:35 -0200

    js e css

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Saudação</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
       @for ($i=0; $i < 100; $i++)
       <h1>OLA   {{$i}} </h1>
       @endfor
+      <script type="text/javascript" src="/js/app.js">
   </body>
 </html>
